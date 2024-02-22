605dad534922318b176b663e4bd99a98b0be690b
Sabata Mofokeng <sabataernestmofokeng@gmail.com>
2024-02-22 12:52:39 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 class Config:
 
     #MONGO String connection
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/dummy")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost/dummy")
 
     # JWT Secret Key
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "")
